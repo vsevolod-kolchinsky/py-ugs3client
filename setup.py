0ce34f3f2b3059647df42ef63775fa1d5902a0c7
Vsevolod Kolchinsky <d@d.kiev.ua>
2016-12-03 13:07:57 +0200

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='py-ugs3client',
       version='0.1',
       description='UGS3 Python client',
+      long_description=readme(),
+      classifiers=[
+                   'Development Status :: 3 - Alpha',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
       url='https://github.com/vsevolod-kolchinsky/py-ugs3client',
       author='Vsevolod Kolchinsky',
       author_email='vsevolod.kolchinsky@gmail.com',
-      license='MIT',
+      license='Apache Software License',
       packages=['ugs3client'],
       install_requires=[
           'requests','cached-property',
