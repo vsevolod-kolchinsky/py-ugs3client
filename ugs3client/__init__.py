4c515b0b8c5ad3215cc154ec09a07e0bc1772b8b
Vsevolod Kolchinsky <d@d.kiev.ua>
2016-12-03 13:43:23 +0200

    checking for response status_code. raising error if not 200.

--- ugs3client/__init__.py
+++ ugs3client/__init__.py
@@ -22,6 +22,11 @@ class UGS3Client(object):
                                 'Accept':'application/json',
                                 }
 
+    def _response_successful(self,response):
+        if 200 != r.status_code:
+            raise UGS3ClientException(r.json())
+        return True
+
     def set_authorization(self,auth_value):
         self.default_headers.update({
                                      'Authorization':auth_value,
@@ -30,34 +35,36 @@ class UGS3Client(object):
     def login(self,**kwargs):
         r = requests.post('{}/auth/token/obtain/'.format(self.ugs3_base_url),
                           data=kwargs,headers=self.default_headers)
-        if 200 != r.status_code:
-            raise UGS3ClientException(r.json())
-        # auth ok:
-        self.set_authorization('JWT {}'.format(r.json()['token']))
-        return r.json()
+        if self._response_successful(r):
+            self.set_authorization('JWT {}'.format(r.json()['token']))
+            return r.json()
 
     @cached_property
     def my_username(self):
         r = requests.get('{}/auth/account/'.format(self.ugs3_base_url),
                          headers=self.default_headers)
-        return r.json()['username']
+        if self._response_successful(r):
+            return r.json()['username']
 
     def find_containers(self,**kwargs):
         r = requests.post('{}/containers/find/'.format(self.ugs3_base_url),
                           data=kwargs,headers=self.default_headers)
-        return r.json()
+        if self._response_successful(r):
+            return r.json()
         
 
     def get_container_payload(self,uuid):
         r = requests.get('{}/containers/{}/render/'.format(self.ugs3_base_url,
                                                            uuid),
                          headers=self.default_headers)
-        return r.json()
+        if self._response_successful(r):
+            return r.json()
 
     def get_container(self,uuid):
         r = requests.get('{}/containers/{}/'.format(self.ugs3_base_url,
                                                     uuid),
                          headers=self.default_headers)
-        return r.json()
+        if self._response_successful(r):
+            return r.json()
 
 
