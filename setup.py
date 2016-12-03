c6bb62f9d20cd6ae6e239b04087f705118c7346e
Vsevolod Kolchinsky <d@d.kiev.ua>
2016-12-03 12:08:37 +0200

    setuptools config

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-ugs3client',
+      version='0.1',
+      description='UGS3 Python client',
+      url='https://github.com/vsevolod-kolchinsky/py-ugs3client',
+      author='Vsevolod Kolchinsky',
+      author_email='d@d.kiev.ua',
+      license='MIT',
+      packages=['ugs3client'],
+      )
