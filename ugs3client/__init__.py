7705a58d857e0b521c4341af7fdfcfe4c4fc4dc0
Vsevolod Kolchinsky <d@d.kiev.ua>
2016-12-03 19:30:51 +0200

    authentication refactored

--- ugs3client/__init__.py
+++ ugs3client/__init__.py
@@ -24,20 +24,20 @@ class UGS3Client(object):
                                 'User-Agent':'{}/{}'.format(self.__class__.__name__,
                                                             VERSION)
                                 }
-        self._authenticated = False
 
-    def _response_successful(self,response):
-        if 200 != response.status_code:
-            raise UGS3ClientException(response.status_code,
-                                      response.json())
-        return True
-    
     def get_headers(self):
         return self.default_headers
     
     def get_response(self,method,url,**kwargs):
         request_func = getattr(requests,method.lower())
         response = request_func(url,data=kwargs,headers=self.get_headers())
+        if 401 == response.status_code:
+            if 'Signature has expired.' == response.json().get('detail',''):
+                if hasattr(self, '_auth_username'):
+                    self.login(username=self._auth_username,
+                               password=self._auth_password)
+                    return request_func(url,data=kwargs,
+                                        headers=self.get_headers())
         if 200 == response.status_code:
             return response
         raise UGS3ClientException(response.status_code,response.json())
@@ -50,11 +50,12 @@ class UGS3Client(object):
     def login(self,**kwargs):
         r = requests.post('{}/auth/token/obtain/'.format(self.ugs3_base_url),
                           data=kwargs,headers=self.default_headers)
-        if self._response_successful(r):
-            for kwarg in kwargs:
-                setattr(self, '_auth_{}'.format(kwarg), kwargs.get(kwarg))
-            self.set_authorization('JWT {}'.format(r.json()['token']))
-            return r.json()
+        if 200 != r.status_code:
+            raise UGS3ClientException(r.status_code,r.json())
+        for kwarg in kwargs:
+            setattr(self, '_auth_{}'.format(kwarg), kwargs.get(kwarg))
+        self.set_authorization('JWT {}'.format(r.json()['token']))
+        return r.json()
 
     @cached_property
     def my_username(self):
