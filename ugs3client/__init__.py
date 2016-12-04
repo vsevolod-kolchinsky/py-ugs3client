a28eea3c988727d99acd2ee777a68fe25a71b8e4
Vsevolod Kolchinsky <d@d.kiev.ua>
2016-12-04 08:30:53 +0200

    pymemcache setup

--- ugs3client/__init__.py
+++ ugs3client/__init__.py
@@ -6,6 +6,7 @@
 
 import requests
 from cached_property import cached_property
+from pymemcache.client.base import Client as pymemcache_client
 
 VERSION='0.4'
 
@@ -14,37 +15,56 @@ class UGS3ClientException(Exception):
 
 
 class UGS3Client(object):
-    ''' minimal client implementation
+    ''' UGS3 client
+    
+    Usage:
+    
+        ugs3 = UGS3Client(host="...",memcache=('localhost',11211))
+        ugs3.find_containers(name="test")
+    
+    set `memcache=None` to disable client-side caching.
+    
     '''
     
-    def __init__(self,host='ugs3.universinet.org'):
+    def __init__(self,host='ugs3.universinet.org',memcache=('localhost',11211)):
         self.ugs3_base_url = 'https://{}'.format(host)
         self.default_headers = {
                                 'Accept':'application/json',
-                                'User-Agent':'{}/{}'.format(self.__class__.__name__,
-                                                            VERSION)
+                                'User-Agent':'{}/{}'.format(
+                                    self.__class__.__name__,VERSION),
                                 }
+        if memcache is not None:
+            self._setup_memcache(memcache)
 
+    def _setup_memcache(self,memcache_cfg):
+        self.memcache = pymemcache_client(memcache_cfg)
+        pass
+    
     def get_headers(self):
         return self.default_headers
     
     def get_response(self,method,url,**kwargs):
         # hash kwargs and check cache for saved response and Last-Modified
         # if present, include in headers 'If-Modified-Since'
+        kwargs_hash = hash(frozenset(kwargs.items()))
         request_func = getattr(requests,method.lower())
-        response = request_func(url,data=kwargs,headers=self.get_headers())
+        request_headers = self.get_headers()
+        response = request_func(url,data=kwargs,headers=request_headers)
         print(response.headers)
-        # obey Not Modified response 304 and return cached value
         if 401 == response.status_code:
+            # is re-authentication required and possible?
             if 'Signature has expired.' == response.json().get('detail',''):
                 if hasattr(self, '_auth_username'):
                     self.login(username=self._auth_username,
                                password=self._auth_password)
                     response = request_func(url,data=kwargs,
                                             headers=self.get_headers())
+        # obey Not Modified response 304 and return cached value
         if 200 == response.status_code:
             # check headers for Last-Modified
             # if present, using kwargs hash save response and Last-Modified value
+            if 'Last-Modified' in response.headers:
+                pass
             return response.json()
         raise UGS3ClientException(response.status_code,response.json())
 
