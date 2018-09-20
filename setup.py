eb9699e29d1ba9521a5bb7694946f5052d64d4b7
Vsevolod Kolchinsky <d@d.kiev.ua>
2018-09-20 21:18:44 +0300

    version DRY

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
+from ugs3client import __version__ as ugs3client_version
+
 
 def readme():
     with open('README.md') as f:
         return f.read()
 
+
 setup(name='py-ugs3client',
-      version='0.9.1',
+      version=ugs3client_version,
       description='UGS3 Python client',
       long_description=readme(),
       classifiers=[
@@ -20,6 +23,5 @@ setup(name='py-ugs3client',
       license='Apache Software License',
       packages=['ugs3client'],
       install_requires=[
-          'requests','cached-property','pymemcache',
-      ],
+          'requests', 'cached-property', 'pymemcache', ],
       )
